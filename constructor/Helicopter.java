dda12707778bc89482a72ead1f0f01507264995e
sanjeevinisb <sanjeevini.xworkz@gmail.com>
2024-07-24 10:10:47 +0530

    upload

---
+++ constructor/Helicopter.java
@@ -0,0 +1,126 @@
+class  Helicopter
+{
+	public Helicopter()
+	{
+		System.out.println("created Helicopter");
+	}
+}
+	class Market
+	{
+    public Market()
+	{
+		System.out.println("created Market");
+	}
+	}
+	class Mall
+	{
+	public Mall()
+	{
+		System.out.println("created Mall");
+	}
+	}
+	class Train
+	{
+	public Train()
+	{
+		System.out.println("created Train");
+	}
+	}
+	class Ring
+	{
+	public Ring()
+	{
+		System.out.println("created Ring");
+	}
+	}
+	class Auto
+	{
+	public Auto()
+	{
+	    System.out.println("created Auto");
+	}
+	}
+	class Auditorium
+	{
+	public Auditorium()
+	{
+        System.out.println("created Auditorium");
+	}
+	}
+	class SpaceShip
+	{
+	public SpaceShip()
+	{
+		System.out.println("created SpaceShip");
+	}
+	}
+	class Alien
+	{
+	public Alien()
+	{
+		System.out.println("created Alien");
+	}
+	}
+	class Bullet
+	{
+	public Bullet()
+	{
+		System.out.println("created Bullet");
+	}
+	}
+	class Galaxy
+	{
+	public Galaxy()
+	{
+		System.out.println("created Galaxy");
+	}
+	}
+	class Spoon
+	{
+	public Spoon()
+	{
+	    System.out.println("created Spoon");
+	}
+	}
+	class BlackHole
+	{
+	public BlackHole()
+	{
+	    System.out.println("created BlackHole");
+	}
+	}
+	class Cake
+	{
+    public Cake()
+	{
+		System.out.println("created Cake");
+	}
+	}
+	class Wrapper
+	{
+	public Wrapper()
+	{
+		System.out.println("created Wrapper");
+	}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
+		
---
+++ constructor/HelicopterRunner.java
@@ -0,0 +1,84 @@
+class  HelicopterRunner
+{
+	public static void main(String[] args)
+	{
+		System.out.println("start main in  HelicopterRunner");
+		new Helicopter();
+		Helicopter helicopter1=new Helicopter();
+		Helicopter helicopter2=new Helicopter();
+		Helicopter helicopter4=new Helicopter();
+		Helicopter helicopter=new Helicopter();
+		new Market();
+		Market market1=new Market();
+		Market market2=new Market();
+		Market market3=new Market();
+		Market market4=new Market();
+		new Mall();
+		Mall mall1=new Mall();
+		Mall mall2=new Mall();
+		Mall mall3=new Mall();
+		Mall mall4=new Mall();
+		new Train();
+		Train train1=new Train();
+		Train train2=new Train();
+		Train train3=new Train();
+		Train train4=new Train();
+		new Ring();
+		Ring ring1=new Ring();
+		Ring ring2=new Ring();
+		Ring ring3=new Ring();
+		Ring ring4=new Ring();
+		new Auto();
+		Auto auto1=new Auto();
+		Auto auto2=new Auto();
+		Auto auto3=new Auto();
+		Auto auto4=new Auto();
+		new Auditorium();
+		Auditorium auditorium1=new Auditorium();
+		Auditorium auditorium2=new Auditorium();
+		Auditorium auditorium3=new Auditorium();
+		Auditorium auditorium4=new Auditorium();
+		new SpaceShip();
+		SpaceShip spaceship1=new SpaceShip();
+		SpaceShip spaceship2=new SpaceShip();
+		SpaceShip spaceship3=new SpaceShip();
+		SpaceShip spaceship4=new SpaceShip();
+		new Alien();
+		Alien alien1=new Alien();
+	    Alien alien2=new Alien();
+		Alien alien3=new Alien();
+		Alien alien4=new Alien();
+		new Bullet();
+		Bullet bullet1=new Bullet();
+		Bullet bullet2=new Bullet();
+		Bullet bullet3=new Bullet();
+		Bullet bullet4=new Bullet();
+		new Galaxy();
+		Galaxy galaxy1=new Galaxy();
+		Galaxy galaxy2=new Galaxy();
+		Galaxy galaxy3=new Galaxy();
+		Galaxy galaxy4=new Galaxy();
+		new Spoon();
+		Spoon spoon1=new Spoon();
+		Spoon spoon2=new Spoon();
+		Spoon spoon3=new Spoon();
+		Spoon spoon4=new Spoon();
+		new BlackHole();
+		BlackHole blackhole1=new BlackHole();
+		BlackHole blackhole2=new BlackHole();
+		BlackHole blackhole3=new BlackHole();
+		BlackHole blackhole4=new BlackHole();
+		new Cake();
+		Cake cake1=new Cake();
+		Cake cake2=new Cake();
+		Cake cake3=new Cake();
+		Cake cake4=new Cake();
+		new Wrapper();
+		Wrapper wrapper1=new Wrapper();
+		Wrapper wrapper2=new Wrapper();
+		Wrapper wrapper3=new Wrapper();
+		Wrapper wrapper4=new Wrapper();
+		System.out.println("end main in  HelicopterRunner");
+		
+	}
+}
