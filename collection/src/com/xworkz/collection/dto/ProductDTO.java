fd12ec4af41a3a288e27defd25a9d302d208ef99
sanjeevinisb <sanjeevini.xworkz@gmail.com>
2024-10-09 13:43:50 +0530

    filter

---
+++ collection/src/com/xworkz/collection/dto/ProductDTO.java
@@ -0,0 +1,94 @@
+package com.xworkz.collection.dto;
+
+import java.io.Serializable;
+
+
+public class ProductDTO implements Serializable,Comparable<ProductDTO>{
+	private int id;
+	private String name;
+	private String vendor ;
+	private double cost;
+	
+	public ProductDTO() {
+	
+	}
+	public ProductDTO(int id, String name, String vendor, double cost) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.vendor = vendor;
+		this.cost = cost;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getVendor() {
+		return vendor;
+	}
+	public void setVendor(String vendor) {
+		this.vendor = vendor;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	@Override
+	public String toString() {
+		return "ProductDTO [id=" + id + ", name=" + name + ", vendor=" + vendor + ", cost=" + cost + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(cost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((vendor == null) ? 0 : vendor.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductDTO other = (ProductDTO) obj;
+		if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (vendor == null) {
+			if (other.vendor != null)
+				return false;
+		} else if (!vendor.equals(other.vendor))
+			return false;
+		return true;
+	}
+	@Override
+	public int compareTo(ProductDTO o) {
+		return Double.compare(this.cost,o.cost);
+	}
+	
+	} 
+	
+
---
+++ collection/src/com/xworkz/collection/runner/ProductRunner.java
@@ -0,0 +1,37 @@
+package com.xworkz.collection.runner;
+
+import java.util.ArrayList; 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import com.xworkz.collection.dto.ProductDTO;
+
+public class ProductRunner {
+
+	public static void main(String[] args) {
+		Collection<ProductDTO> products = new ArrayList<ProductDTO>();
+		products.add(new ProductDTO (1,"Mobile","Samsung",25000));
+		products.add(new ProductDTO (2,"Laptop","Lenovo",28000));    
+		products.add(new ProductDTO (3,"Chocolate","Alpenliebe",67000));
+		products.add(new ProductDTO (4,"Watch","Noise",2000));
+		products.add(new ProductDTO (5,"Shoes","Nike",22000));
+		products.add(new ProductDTO (6,"Tv","Lg",34000));
+		products.add(new ProductDTO (7,"Refrigerator","Whirlpool",17000));
+		products.add(new ProductDTO (8,"Bag","Feather",1500));
+		products.add(new ProductDTO (9,"Bottle","Borosil",5000));
+		products.add(new ProductDTO (10,"Lunch Box","Milton",1400));
+		
+		
+		products.forEach((productDTO)->System.out.println(productDTO));
+		 
+		System.out.println("********sort*********");
+		
+		 products
+		 .stream()
+         .filter(dto -> dto.getCost() > 5000 && dto.getCost() < 30000) 
+         .sorted((dto1, dto2) -> Double.compare(dto2.getCost(), dto1.getCost()))
+         .collect(Collectors.toList())
+         .forEach(dto -> System.out.println(dto));			 
+	}
+
+}
