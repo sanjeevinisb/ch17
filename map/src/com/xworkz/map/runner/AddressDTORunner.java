af05f78ec73dabc718b09b325ecdc193c3d19ff2
sanjeevinisb <sanjeevini.xworkz@gmail.com>
2024-11-08 08:31:15 +0530

    map and loop

--- map/src/com/xworkz/map/runner/AddressDTORunner.java
+++ map/src/com/xworkz/map/runner/AddressDTORunner.java
@@ -1,22 +1,24 @@
 package com.xworkz.map.runner;
 
-import java.util.Collection;
+import java.util.Collection; 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 import com.xworkz.map.dto.AddressDTO;
+import com.xworkz.map.dto.Code;
+import com.xworkz.map.dto.CountryDTO;
 
 public class AddressDTORunner {
 
 	public static void main(String[] args) {
 
 		Map<String, AddressDTO> map = new HashMap<>();
-		map.put("Gagan", new AddressDTO(90785434517L, "RR nagar", "Bangalore", "Karnataka",new CountryDTO("India", Code.INDIA)));
-		map.put("Akanksha",new AddressDTO(98236860655L, "kavya", "kochi", " kerala", new CountryDTO("India", Code.INDIA)));
-		map.put("praveen",new AddressDTO(9989267789L, "vrushank", "pune", "Karnataka", new CountryDTO("India", Code.INDIA)));
-		map.put("sushmita",new AddressDTO(98776434567L, "ramesh", "Kolar", "Karnataka", new CountryDTO("India", Code.INDIA)));
-		map.put("preetik",new AddressDTO(98877645678L, "BT Nagar", "manglore", "Karnataka", new CountryDTO("India", Code.INDIA)));
+		map.put("Sanjeevini", new AddressDTO(90785434517L, "KalyanNagar", "Bangalore", "Karnataka",new CountryDTO("India", Code.INDIA)));
+		map.put("Krishav",new AddressDTO(98236860655L, "Bharati", "kochi", " kerala", new CountryDTO("India", Code.INDIA)));
+		map.put("Nikhita",new AddressDTO(9989267789L, "Aarati", "pune", "Karnataka", new CountryDTO("India", Code.INDIA)));
+		map.put("Sneha",new AddressDTO(98776434567L, "Poorna", "Kolar", "Karnataka", new CountryDTO("India", Code.INDIA)));
+		map.put("Ashwini",new AddressDTO(98877645678L, "Rajajinagar", "manglore", "Karnataka", new CountryDTO("India", Code.INDIA)));
 		
 		Set<String> keys = map.keySet();
 		keys.forEach(System.out::println);
